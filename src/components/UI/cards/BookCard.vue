9677db84932c7ca3f8844a0a515fecbce3b8c9b1
dmitry.b <dmitry.b@zala.com.au>
2024-01-28 16:01:12 +0200

    add images

--- src/components/UI/cards/BookCard.vue
+++ src/components/UI/cards/BookCard.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <div class="max-w-full w-[350px] p-4 border-black border-[1px] rounded-[5px] cursor-pointer">
-            <div class="flex items-center justify-between">
+            <img :src="randomImage()" alt="image book"/>
+            <div class="flex items-center justify-between mt-2">
                 <p class="font-bold text-xl">{{ book.name }}</p>
                 <slot name="card-upper"></slot>
             </div>
@@ -16,10 +17,17 @@
 </template>
 
 <script setup>
-import { useBookStore } from '../../../store/store';
 import BookRatingStars from '../stars/BookRatingStars.vue';
-
-const store = useBookStore();
+import rulesForLife from "../../../assets/bookImages/12_rules_for_life.jpg";
+import atomicHabits from "../../../assets/bookImages/Atomic Habits.jpg";
+import badass from "../../../assets/bookImages/badass.jpg";
+import bestSelf from "../../../assets/bookImages/Best Self.jpg";
+import girlWashYourFace from "../../../assets/bookImages/Girl, Wash Your Face.jpg";
+import habits from "../../../assets/bookImages/The 7 Habits of Highly Effective People.jpg";
+import alchemist from "../../../assets/bookImages/The Alchemist.jpg";
+import fourAgreements from "../../../assets/bookImages/The Four Agreements.jpg";
+import subtleArt from "../../../assets/bookImages/The Subtle Art of Not Giving a F*ck.jpg";
+import thinkingFastAndSlow from "../../../assets/bookImages/Thinking Fast and Slow.jpg";
 
 const props = defineProps({
     book: {
@@ -27,4 +35,8 @@ const props = defineProps({
         required: true
     }
 })
+
+const bookImages = [rulesForLife, atomicHabits, badass, bestSelf, girlWashYourFace, habits, alchemist, fourAgreements, subtleArt, thinkingFastAndSlow];
+
+const randomImage = () => bookImages[Math.floor(Math.random() * bookImages.length)];
 </script>
